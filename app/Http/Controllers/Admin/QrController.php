746b67ccfbae3a68d8d809050754363857d3008c
Fahrih Fatahilah <ffatahilah@bankbjb.co.id>
2026-09-11 17:03:54 +0700

    fixing vuln

--- app/Http/Controllers/Admin/QrController.php
+++ app/Http/Controllers/Admin/QrController.php
@@ -13,27 +13,27 @@ class QrController extends Controller
 
     public function show(Wedding $wedding, Guest $guest)
     {
-        $this->authorize('manageGuests', $wedding);
+        $this->authorize('viewGuests', $wedding);
         abort_if($guest->wedding_id !== $wedding->id, 403);
 
         $svg = $this->qrService->generate($guest);
 
         return response($svg, 200, [
-            'Content-Type'        => 'image/svg+xml',
+            'Content-Type' => 'image/svg+xml',
             'Content-Disposition' => 'inline',
         ]);
     }
 
     public function download(Wedding $wedding, Guest $guest)
     {
-        $this->authorize('manageGuests', $wedding);
+        $this->authorize('viewGuests', $wedding);
         abort_if($guest->wedding_id !== $wedding->id, 403);
 
         $png = $this->qrService->generatePng($guest);
-        $filename = 'qr-' . str($guest->name)->slug() . '.png';
+        $filename = 'qr-'.str($guest->name)->slug().'.png';
 
         return response($png, 200, [
-            'Content-Type'        => 'image/png',
+            'Content-Type' => 'image/png',
             'Content-Disposition' => "attachment; filename=\"{$filename}\"",
         ]);
     }
